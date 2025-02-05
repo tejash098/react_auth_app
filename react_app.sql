a94c66b8ce029bc4d80e246f2c32b578f546f177
Tejash kumar singh <122388189+tejash098@users.noreply.github.com>
2025-02-05 22:35:09 +0530

    Add files via upload

---
+++ react_app.sql
@@ -0,0 +1,19 @@
+CREATE DATABASE react_app;
+
+USE react_app;
+
+CREATE TABLE `remarks` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `text` text NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) 
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) 
